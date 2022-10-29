e9715d0cfb6ef96b7d6f5f0e636b0dc58f1a08c0
Carl0s2002 <fdrlaca@hotmail.com>
2022-10-29 13:20:48 +0300

    6. lab hazi

--- lab06/Polynomial.cpp
+++ lab06/Polynomial.cpp
@@ -14,9 +14,9 @@ Polynomial::Polynomial(int degree, const double *coefficients) {
 
 ostream &operator<<(ostream &out, const Polynomial &what) {
     for ( int i = 0 ; i < what.capacity-1 ; ++i ){
-        out << what.coefficients[i] << "x^" << what.capacity - 1 - i << "+" ;
+        out << "(" << what.coefficients[i] << "x^" << what.capacity - 1 - i << ")"  << "+" ;
     }
-    out << what.coefficients[what.capacity-1 ] ;
+    out << "(" << what.coefficients[what.capacity-1 ] << ")" ;
     return out ;
 }
 
@@ -44,7 +44,7 @@ Polynomial::Polynomial(Polynomial &&that) {
     this->capacity = that.capacity ;
     this->coefficients = that.coefficients ;
     that.capacity = 0 ;
-    delete [] that.coefficients ;
+    that.coefficients = nullptr ;
 }
 
 Polynomial Polynomial::derivative() const {
@@ -53,6 +53,7 @@ Polynomial Polynomial::derivative() const {
         p.coefficients[i] *= degree()-i ;
     }
     p.coefficients[this->capacity-1] = 0 ;
+    p.capacity-- ;
     return p ;
 }
 
@@ -61,7 +62,54 @@ double Polynomial::operator[](int index) const {
 }
 
 Polynomial operator-(const Polynomial &a) {
+    for ( int i = 0 ; i < a.capacity ; ++i ) {
+        a.coefficients[i] *= -1 ;
+    }
+    return a ;
+}
 
+Polynomial operator+(const Polynomial &a, const Polynomial &b) {
+    if ( a.capacity > b.capacity ) {
+        Polynomial addition( a.degree() , a.coefficients ) ;
+        for ( int i = a.degree() - b.degree() ; i < a.capacity ; ++i ) {
+             addition.coefficients[i] += b.coefficients[i-(a.degree()-b.degree())] ;
+        }
+        return addition ;
+    }
+        Polynomial addition( b.degree() , b.coefficients ) ;
+        for ( int i = b.degree() - a.degree() ; i < b.capacity ; ++i ) {
+            addition.coefficients[i] += a.coefficients[i-(b.degree()-a.degree())] ;
+        }
+    return addition ;
+}
+
+Polynomial operator-(const Polynomial &a, const Polynomial &b) {
+    if ( a.capacity > b.capacity ) {
+        Polynomial substract(a.degree() , a.coefficients ) ;
+        for ( int i = a.degree() - b.degree() ; i < a.capacity ; ++i ) {
+            substract.coefficients[i] -= b.coefficients[i-(a.degree()-b.degree())] ;
+        }
+        return substract ;
+    }
+    Polynomial substract(b.degree() , b.coefficients ) ;
+    for ( int i = b.degree() - a.degree() ; i < b.capacity ; ++i ) {
+        substract.coefficients[i] -= a.coefficients[i-(b.degree()-a.degree())] ;
+    }
+    return substract ;
+}
+
+Polynomial operator*(const Polynomial &a, const Polynomial &b) {
+    double helper[a.degree()+b.degree()+1] ;
+    Polynomial multiplication( a.degree()+b.degree() , helper ) ;
+    for ( int i = 0 ; i < multiplication.capacity ; ++i ) {
+        multiplication.coefficients[i] = 0 ;
+    }
+    for ( int i = 0 ; i < a.capacity ; ++i ) {
+        for ( int j = 0 ; j < b.capacity ; ++j ) {
+            multiplication.coefficients[i+j] += a.coefficients[i] * b.coefficients[j] ;
+        }
+    }
+    return multiplication ;
 }
 
 
--- lab06/main_06.cpp
+++ lab06/main_06.cpp
@@ -2,10 +2,55 @@
 #include "Polynomial.h"
 using namespace std ;
 int main() {
+    ///p1 kiiratasa
     double coef[]{ 1 , 2 , 1 } ;
     Polynomial p1( 2 , coef );
-    cout << "p1:" << p1 << endl ;
-    cout << "Degree(p1):" << p1.degree() << endl ;
-    cout << "Result(p1) if x = 2: " << p1.evaluate(2) ;
+    cout << "p1:" << p1 << endl << endl  ;
+
+    ///p1 fokszama
+    cout << "Degree(p1):" << p1.degree() << endl << endl ;
+
+    ///p1 eredmenye x-et ismerve
+    int x ;
+    cout << "Choose a value for x:" ;
+    cin >> x;
+    cout << "Result(p1): " << p1.evaluate(x) << endl << endl ;
+
+    ///p1 derivaltja
+    Polynomial p2 = p1.derivative() ;
+    cout << "Derivative(p1):";
+    cout << p2 << endl << endl ;
+
+    ///Copy konstruktor
+    cout << "Copy constructor:" << endl;
+    Polynomial p3 = p1 ;
+    cout << "p3 = p1 = " << p3 << endl << endl ;
+
+    ///Move konstruktor
+    cout << "Move constructor:" << endl;
+    Polynomial p4 = move(p2) ;
+    cout << "Moved from p2 to p4; p4 = " << p4 << endl << endl;
+
+    ///Index operator
+    cout << "Index operator:" << endl ;
+    for ( int i = 0 ; i < p1.degree()+1 ; ++i ) {
+        cout << "p1[" << i << "]= " << p1[i] << endl  << endl ;
+    }
+
+    /// "-" Operator(tagadas)
+    cout << "'-' Operator: " << endl ;
+    cout << "-p1 = " << -p1 << endl << endl ;
+
+    /// "+" Operator
+    cout << "'+' Operator: " << endl ;
+    cout << "p1 + p4 = " << p1+p4 << endl << endl ;
+
+    /// "-" Operator
+    cout << "'-' Operator:" << endl ;
+    cout << "p1 - p4 = " << p1 - p4 << endl << endl ;
+
+    /// "*" Operator
+    cout << "'*' Operator:" << endl ;
+    cout << "p1 * p4 = " << p1 * p4 ;
     return 0;
 }
